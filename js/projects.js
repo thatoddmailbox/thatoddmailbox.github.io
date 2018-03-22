627156acf4e3545120d2dbb05cfb25b2c9d374d3
Alex Studer <astuder@notetoscreen.com>
2018-03-22 00:27:16 -0400

    change 'read more' to 'read less' when clicked

--- js/projects.js
+++ js/projects.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $(".project-expand-trigger .btn").click(function() {
         var $button = $(this);
         var $buttonIcon = $button.find(".fa");
+        var $buttonActionText = $button.find("span");
         var $projectInfo = $button.parent().parent();
         var $project = $projectInfo.parent().parent().parent().parent();
 
@@ -9,6 +10,7 @@ $(document).ready(function() {
             // hide everything
 
             $button.removeClass("active");
+            $buttonActionText.text("more");
             $buttonIcon.removeClass("fa-chevron-up").addClass("fa-chevron-down");
             $project.removeClass("selected");
             $projectInfo.find(".project-expand-content").css("max-height", "0");
@@ -18,6 +20,7 @@ $(document).ready(function() {
             $(".project.selected .project-expand-trigger .btn").click();
 
             $button.addClass("active");
+            $buttonActionText.text("less");
             $buttonIcon.removeClass("fa-chevron-down").addClass("fa-chevron-up");
             $project.addClass("selected");
             $projectInfo.find(".project-expand-content").css("max-height", "500px");
