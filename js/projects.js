27aed3fbc6e3187bd56eee14a52ac311fc08967d
Alex Studer <astuder@notetoscreen.com>
2018-03-22 00:25:11 -0400

    add a read more button to projects, allowing you to expand their entry and see more details

---
+++ js/projects.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $(".project-expand-trigger .btn").click(function() {
+        var $button = $(this);
+        var $buttonIcon = $button.find(".fa");
+        var $projectInfo = $button.parent().parent();
+        var $project = $projectInfo.parent().parent().parent().parent();
+
+        if ($project.hasClass("selected")) {
+            // hide everything
+
+            $button.removeClass("active");
+            $buttonIcon.removeClass("fa-chevron-up").addClass("fa-chevron-down");
+            $project.removeClass("selected");
+            $projectInfo.find(".project-expand-content").css("max-height", "0");
+        } else {
+            // show everything
+
+            $(".project.selected .project-expand-trigger .btn").click();
+
+            $button.addClass("active");
+            $buttonIcon.removeClass("fa-chevron-down").addClass("fa-chevron-up");
+            $project.addClass("selected");
+            $projectInfo.find(".project-expand-content").css("max-height", "500px");
+        }
+    });
+});
