2ec6c4f8b397b9acb33070c41f2c4a0d3d7d3f8b
Alex Studer <alex@studer.dev>
2022-09-29 13:25:08 -0400

    scripts/cibuild: set assume-extension correctly

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer ./_site --root-dir ./_site/ --assume-extension --url-ignore "/(.*)iotaseed(.*)/,/(.*)[Nn]orbert(.*)/,/(.*)archive(.*)/,/(.*)iotabalance(.*)/,/(.*)source.android(.*)/" --typhoeus '{"headers": {"User-Agent": "Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/80.0.3987.149 Safari\/537.36 htpf/1"}}'
+bundle exec htmlproofer ./_site --root-dir ./_site/ --assume-extension .html --url-ignore "/(.*)iotaseed(.*)/,/(.*)[Nn]orbert(.*)/,/(.*)archive(.*)/,/(.*)iotabalance(.*)/,/(.*)source.android(.*)/" --typhoeus '{"headers": {"User-Agent": "Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/80.0.3987.149 Safari\/537.36 htpf/1"}}'
