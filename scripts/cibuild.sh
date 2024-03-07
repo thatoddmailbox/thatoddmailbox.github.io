60bc7fdfd653b7ddf2f57b9c759ee9a0ff2a1b85
Alex Studer <alex@studer.dev>
2024-03-06 22:28:29 -0500

    scripts/cibuild: add reddit to ignore list
    seems like they are blocking requests from github actions

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer ./_site --root-dir ./_site/ --assume-extension .html --no-check-external-hash --ignore-urls "/(.*)iotaseed(.*)/,/(.*)[Nn]orbert(.*)/,/(.*)archive(.*)/,/(.*)iotabalance(.*)/,/(.*)source.android(.*)/,/(.*)die.net(.*)/" --typhoeus '{"headers": {"User-Agent": "Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/117.0.0.0 Safari\/537.36 htpf/1"}}'
+bundle exec htmlproofer ./_site --root-dir ./_site/ --assume-extension .html --no-check-external-hash --ignore-urls "/(.*)iotaseed(.*)/,/(.*)[Nn]orbert(.*)/,/(.*)archive(.*)/,/(.*)iotabalance(.*)/,/(.*)source.android(.*)/,/(.*)die.net(.*),/(.*)reddit.com(.*)/" --typhoeus '{"headers": {"User-Agent": "Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/117.0.0.0 Safari\/537.36 htpf/1"}}'
