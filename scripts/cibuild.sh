00f6f8d4d8a8974b80a4df4e3a58a872fd35bb51
Alex Studer <alex@studer.dev>
2025-11-11 19:33:10 -0500

    scripts/cibuild: don't check npmjs.com
    some cloudflare block means the script fails to fetch the url

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -6,5 +6,5 @@ bundle exec htmlproofer ./_site \
 	--root-dir ./_site/ \
 	--assume-extension .html \
 	--no-check-external-hash \
-	--ignore-urls "/(.*)iotaseed(.*)/,/(.*)[Nn]orbert(.*)/,/(.*)archive(.*)/,/(.*)iotabalance(.*)/,/(.*)source.android(.*)/,/(.*)die.net(.*)/,/(.*)reddit.com(.*)/,/(.*)4pda.ru(.*)/,/(.*)osmocom.org(.*)/,/(.*)alcatelmobile.com(.*)/" \
+	--ignore-urls "/(.*)iotaseed(.*)/,/(.*)[Nn]orbert(.*)/,/(.*)archive(.*)/,/(.*)iotabalance(.*)/,/(.*)source.android(.*)/,/(.*)die.net(.*)/,/(.*)reddit.com(.*)/,/(.*)4pda.ru(.*)/,/(.*)osmocom.org(.*)/,/(.*)alcatelmobile.com(.*)//,/(.*)npmjs.com(.*)/" \
 	--typhoeus '{"headers": {"User-Agent": "Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/129.0.0.0 Safari\/537.36 htpf/1"}}'
