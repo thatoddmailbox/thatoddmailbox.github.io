037f52de3206db84f3835a40ddff723b7e3c97e8
Alex Studer <alex@studer.dev>
2026-08-21 23:40:41 -0400

    scripts/cibuild: for htmlproofer, ignore status codes that seem related to ip blocking

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -2,9 +2,14 @@
 set -e # halt script on error
 
 bundle exec jekyll build
+
+# many sites seem to block datacenter IPs now, I assume because of AI scrapers :(
+# solution for that is to ignore 403, 418, 429 errors but it's a bit of a hack
+# at least we will still catch broken domains and 404s!
 bundle exec htmlproofer ./_site \
 	--root-dir ./_site/ \
 	--assume-extension .html \
 	--no-check-external-hash \
-	--ignore-urls "/(.*)iotaseed(.*)/,/(.*)[Nn]orbert(.*)/,/(.*)archive(.*)/,/(.*)iotabalance(.*)/,/(.*)source.android(.*)/,/(.*)die.net(.*)/,/(.*)reddit.com(.*)/,/(.*)4pda.ru(.*)/,/(.*)osmocom.org(.*)/,/(.*)alcatelmobile.com(.*)//,/(.*)npmjs.com(.*)/" \
+	--ignore-status-codes "403,418,429" \
+	--ignore-urls "/(.*)iotaseed(.*)/,/(.*)[Nn]orbert(.*)/,/(.*)iotabalance(.*)/,/(.*)alcatelmobile.com(.*)/,/(.*)4pda.ru(.*)/,/(.*)source.android(.*)/,/(.*)osmocom.org(.*)/,/(.*)archive(.*)/" \
 	--typhoeus '{"headers": {"User-Agent": "Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/149.0.0.0 Safari\/537.36 htpf/1"}}'
