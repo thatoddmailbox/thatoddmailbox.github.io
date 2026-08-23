03986a3c5e60383e1825ba50c9b415c10899e3e7
Alex Studer <alex@studer.dev>
2026-08-23 15:41:11 -0400

    scripts/cibuild: add another entry to the ignore list

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -12,5 +12,5 @@ bundle exec htmlproofer ./_site \
 	--assume-extension .html \
 	--no-check-external-hash \
 	--ignore-status-codes "403,418,429,999" \
-	--ignore-urls "/(.*)iotaseed(.*)/,/(.*)[Nn]orbert(.*)/,/(.*)iotabalance(.*)/,/(.*)alcatelmobile.com(.*)/,/(.*)4pda.ru(.*)/,/(.*)source.android(.*)/,/(.*)osmocom.org(.*)/,/(.*)archive(.*)/" \
+	--ignore-urls "/(.*)iotaseed(.*)/,/(.*)[Nn]orbert(.*)/,/(.*)iotabalance(.*)/,/(.*)alcatelmobile.com(.*)/,/(.*)4pda.ru(.*)/,/(.*)source.android(.*)/,/(.*)osmocom.org(.*)/,/(.*)archive(.*)/,/(.*)freedesktop(.*)/" \
 	--typhoeus '{"headers": {"User-Agent": "Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/149.0.0.0 Safari\/537.36 htpf/1"}}'
