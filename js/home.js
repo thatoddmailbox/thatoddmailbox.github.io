47a21c1bd7546e666e715981856364524a878cd0
Alex Studer <alex@studer.dev>
2026-08-23 23:02:12 -0400

    remove js file that is no longer needed

--- js/home.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$(".hero").click(function() {
-		if ($(this)[0].tagName != "a") {
-			var link = $(this).find("a");
-			window.location.assign(link.attr("href"));
-		}
-	});
-});
